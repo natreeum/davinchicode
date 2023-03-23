039f8a756c1b348c33e93d00e0a21d4ba7059fbe
natreeum <projectkmscom@gmail.com>
2023-03-23 23:34:33 +0900

    commit

--- client/src/network.js
+++ client/src/network.js
@@ -1,5 +1,6 @@
 const network = {
-  ip: 'http://localhost',
+  // ip: 'http://localhost',
+  ip: 'http://192.168.0.21',
   port: '3001',
 };
 
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 const server = http.createServer(app);
 const PORT = 3001;
